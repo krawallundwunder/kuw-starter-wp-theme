393a22ed989ac6a95a5b72ea5f94e73ac150f076
Florian <flomuench.dd@gmail.com>
2021-01-20 13:15:56 +0100

    add simple header

--- header.php
+++ header.php
@@ -20,9 +20,10 @@
 	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
 				rel="stylesheet">
 
-	<script src="svg-inject.min.js"></script>
+	<script src="<?php echo get_template_directory_uri() ?>/assets/js/svg-inject.min.js"></script>
 
 	<?php wp_head(); ?>
+	<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 </head>
 
 <body <?php body_class(); ?>>
@@ -31,40 +32,43 @@
 	<a class="skip-link screen-reader-text"
 		 href="#primary"><?php esc_html_e('Skip to content', 'phmu-starter-wp-theme'); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if (is_front_page() && is_home()) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a>
-				</h1>
-			<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a>
+	<header id="masthead" class="site-header" x-data="mobileMenu()">
+		<div class="container pr-4 pl-4">
+			<div class="site-branding">
+				<p class="site-title">
+					<a
+						href="<?php echo esc_url(home_url('/')); ?>"
+						rel="home"
+					>
+						<?php bloginfo('name'); ?>
+					</a>
 				</p>
-			<?php
-			endif;
-			$phmu_starter_wp_theme_description = get_bloginfo('description', 'display');
-			if ($phmu_starter_wp_theme_description || is_customize_preview()) :
-				?>
-				<p
-					class="site-description"><?php echo $phmu_starter_wp_theme_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-					?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+			</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu"
-							aria-expanded="false"><?php esc_html_e('Primary Menu', 'phmu-starter-wp-theme'); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id' => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
+			<nav id="site-navigation" class="main-navigation">
+				<button
+					class="menu-toggle"
+					aria-controls="menu-mobile"
+					aria-expanded="false"
+					@click="toggle"
+				>
+					<?php esc_html_e('Primary Menu', 'phmu-starter-wp-theme'); ?>
+				</button>
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'menu-1',
+						'menu_class' => 'menu-desktop'
+					)
+				);
+				?>
+				<div class="menu-mobile" x-show="isOpen()"><?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'menu-1',
+							'menu_id' => 'menu-mobile',
+						)
+					);
+					?></div>
+			</nav><!-- #site-navigation --></div>
 	</header><!-- #masthead -->
