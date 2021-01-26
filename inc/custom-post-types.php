4b31aad9d2030c88f4a671c729d858723f2edb11
Florian <flomuench.dd@gmail.com>
2021-01-26 20:31:01 +0100

    fix post type creation

--- inc/custom-post-types.php
+++ inc/custom-post-types.php
@@ -6,19 +6,24 @@
  */
 
 // TODO: delete this or use this to create a real custom post type
-phmu_starter_wp_theme_register_post_type('sample-type',
-	// CPT Options
-	array(
-		'labels' => array(
-			'name' => __('Sample types'),
-			'singular_name' => __('Sample type')
-		),
-		'public' => true,
-		'publicly_queryable' => true,
-		'has_archive' => true,
-		'rewrite' => array('slug' => 'sample-type'),
-		'show_in_rest' => true,
-		'rest_base' => 'sample-type',
-		'supports' => array('title', 'editor')
-	)
-);
+function phmu_starter_wp_theme_create_post_types()
+{
+	register_post_type('sample-type',
+		// CPT Options
+		array(
+			'labels' => array(
+				'name' => __('Sample types'),
+				'singular_name' => __('Sample type')
+			),
+			'public' => true,
+			'publicly_queryable' => true,
+			'has_archive' => true,
+			'rewrite' => array('slug' => 'sample-type'),
+			'show_in_rest' => true,
+			'rest_base' => 'sample-type',
+			'supports' => array('title', 'editor')
+		)
+	);
+}
+
+add_action('init', 'phmu_starter_wp_theme_create_post_types');
