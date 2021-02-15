0afd47d569eeca2629e55217501abc5e85854b61
Florian <flomuench.dd@gmail.com>
2021-02-15 10:31:51 +0100

    add testimonial block

--- inc/acf-blocks.php
+++ inc/acf-blocks.php
@@ -22,12 +22,52 @@ function acf_blocks_init()
 	// Check function exists.
 	if (function_exists('acf_register_block_type')) {
 
-		// Page Header.
+		// Testimonial
 		acf_register_block_type(array(
-			'name'              => 'sample-block',
-			'title'             => __('Sample Block'),
-			'description'       => __('Just a sample block. Delete it after creating the first real block.'),
-			'render_template'   => 'template-parts/acf-blocks/sample-block.php',
+			'name'              => 'testimonial',
+			'title'             => __('Testimonial'),
+			'description'       => __('A simple testimonial block'),
+			'render_template'   => 'template-parts/acf-blocks/testimonial.php',
+			'category'          => 'phmu-blocks',
+			'mode'              => 'edit',
+		));
+
+		// Team section
+		acf_register_block_type(array(
+			'name'              => 'team-section',
+			'title'             => __('Team section'),
+			'description'       => __('A simple team section'),
+			'render_template'   => 'template-parts/acf-blocks/team-section.php',
+			'category'          => 'phmu-blocks',
+			'mode'              => 'edit',
+		));
+
+		// CTA section
+		acf_register_block_type(array(
+			'name'              => 'cta-section',
+			'title'             => __('CTA section'),
+			'description'       => __('A simple CTA section'),
+			'render_template'   => 'template-parts/acf-blocks/cta-section.php',
+			'category'          => 'phmu-blocks',
+			'mode'              => 'edit',
+		));
+
+		// Blog section
+		acf_register_block_type(array(
+			'name'              => 'blog-section',
+			'title'             => __('Blog section'),
+			'description'       => __('A simple blog section'),
+			'render_template'   => 'template-parts/acf-blocks/blog-section.php',
+			'category'          => 'phmu-blocks',
+			'mode'              => 'edit',
+		));
+
+		// Logo cloud
+		acf_register_block_type(array(
+			'name'              => 'logo-cloud',
+			'title'             => __('Logo cloud'),
+			'description'       => __('A simple logo cloud'),
+			'render_template'   => 'template-parts/acf-blocks/logo-cloud.php',
 			'category'          => 'phmu-blocks',
 			'mode'              => 'edit',
 		));
---
+++ template-parts/acf-blocks/testimonial.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Block: Welcome Text
+ *
+ * @package Advelio_WP_Theme
+ */
+
+$quote = get_field('tm_quote');
+$author_name = get_field('tm_author_name');
+$author_position = get_field('tm_author_position');
+$author_img = get_field('tm_author_img');
+$company_name = get_field('tm_company_name');
+$company_logo = get_field('tm_company_logo');
+?>
+
+<div class="acf-block testimonial">
+	<div class="container pr-4 pl-4">
+		<img class="testimonial__logo" src="<?php echo $company_logo ?>" alt="<?php echo $company_name ?>"/>
+		<blockquote class="mt-10 ml-0 mb-0 mr-0">
+			<div class="testimonial__quote">
+				<p>
+					&ldquo;<?php echo $quote ?>&rdquo;
+				</p>
+			</div>
+			<footer class="mt-8">
+				<div class="testimonial__author-img-wrapper">
+					<img class="testimonial__author-img" src="<?php echo $author_img ?>" alt="<?php echo $author_name ?>"/>
+				</div>
+				<div class="testimonial__author">
+					<div class="testimonial__author-name"><?php echo $author_name ?></div>
+
+					<svg class="testimonial__slash" fill="currentColor" viewBox="0 0 20 20">
+						<path d="M11 0h3L9 20H6l5-20z" />
+					</svg>
+
+					<div class="testimonial__author-position"><?php echo $author_position ?>, <?php echo $company_name ?></div>
+				</div>
+			</footer>
+		</blockquote>
+	</div>
+</div>
